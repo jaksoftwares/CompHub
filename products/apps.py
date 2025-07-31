e68031088c654d3b445118db4c838e806cd7b0c8
OmbongiFelix <fombongi11@gmail.com>
2025-07-31 12:52:07 +0300

    shop profiles

---
+++ products/apps.py
@@ -0,0 +1,8 @@
+#products/apps.py
+# Vendor-specific functionality, shop profiles, and verification
+from django.apps import AppConfig
+
+
+class ProductsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'products'
