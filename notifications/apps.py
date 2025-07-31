8ab1f68bfb841ffa343a88964275225998746da5
OmbongiFelix <fombongi11@gmail.com>
2025-07-31 12:49:52 +0300

    system alerts, messaging and communication

---
+++ notifications/apps.py
@@ -0,0 +1,8 @@
+# notifications/apps.py
+# System alerts, messaging, and communication
+from django.apps import AppConfig
+
+
+class NotificationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'notifications'
